3ce74350abc157a83e71ba6ef3cad7838d937054
COPART\prchalasan <pranay.chalasani@copart.com>
2017-08-29 15:23:58 -0500

    project setup

---
+++ src/main/java/com/springbootexample/SpringBootEmployeeExample.java
@@ -0,0 +1,18 @@
+package com.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by prchalasan on 8/29/2017.
+ */
+
+@SpringBootApplication
+public class SpringBootEmployeeExample {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run( SpringBootEmployeeExample.class, args);
+    }
+}
